f829be4c0f1609040ab031f03e1d6b0727e91958
Harshit Kapadia <108323841+HARSHIT05060@users.noreply.github.com>
2024-12-04 15:13:30 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 
 //Define mongoDB database connection URL
 
-// const mongoURL = 'mongodb://127.0.0.1:27017/myDatabase'  // replace myDatabase name with your database name
-const mongoURL = 'mongodb+srv://harshitkapadia563:harshit0506@hotel.ll4m6.mongodb.net/hotel?retryWrites=true&w=majority'
+const mongoURL = 'mongodb://127.0.0.1:27017/myDatabase'  // replace myDatabase name with your database name
 
 mongoose.connect(mongoURL, {
     // useNewUrlParser: true,
     // useUnifiedTopology: true,     // default passing otherwise you get some error messages at mongodb server
-    serverSelectionTimeoutMS: 30000,
 })
 
 //Get the default connection
